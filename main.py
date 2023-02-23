8f152457e79c3d316598e461491d25bfac80988d
Sean Betts <seanbetts@icloud.com>
2023-02-23 20:47:18 +0000

    Updated to Entity Memory example

--- main.py
+++ main.py
@@ -2,14 +2,47 @@
 import streamlit as st
 from streamlit_chat import message
 
-from langchain.chains import ConversationChain
-from langchain.llms import OpenAI
+from langchain import OpenAI, ConversationChain, PromptTemplate
+from langchain.chains.conversation.memory import ConversationEntityMemory
+from pydantic import BaseModel
+from typing import List, Dict, Any
 
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
-    chain = ConversationChain(llm=llm)
+    template = """
+
+        You are an enthusiastic conversational AI bot that remembers previous conversations.
+
+        Given the following information, answer the question from the user using only that information. If you are unsure and the answer is not available, reply with "Sorry, I don't know how to help with that.”
+
+        Context:
+        {entities}
+
+        Current conversation: {history}
+
+        Human: {input}
+
+        AI:
+
+    """
+    
+    prompt = PromptTemplate(
+    input_variables=["entities", "history", "input"],
+    template=template,
+    )
+    
+    llm = OpenAI(
+    temperature=0, 
+    openai_api_key=OPENAI_API_KEY
+    )
+    
+    chain = ConversationChain(llm=llm, 
+    verbose=False,
+    prompt=prompt,
+    memory=ConversationEntityMemory(llm=llm)
+    )
+    
     return chain
 
 chain = load_chain()
